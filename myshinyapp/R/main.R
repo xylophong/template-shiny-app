0b7efe93dc712114b3e3f88b68ef36ff7bea1781
dpnguyen <dinhphongng@gmail.com>
2025-11-26 17:30:46 +0000

    fix path

--- myshinyapp/R/main.R
+++ myshinyapp/R/main.R
@@ -1,6 +1,6 @@
 
 runApp <- function(){
-  myshinyapp::populate_table(bucket = "dpnguyen", object = "quakes.csv")
+  myshinyapp::populate_table(bucket = "dpnguyen", object = "diffusion/quakes.csv")
   appDir <- system.file("app", package = "myshinyapp")
   shiny::runApp(appDir, display.mode = "normal")
 }
